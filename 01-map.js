3528acd547fae805366517cb094218336e17db97
NirabHossain <nirabamth@gmail.com>
2021-05-22 13:36:33 +0600

    Many

---
+++ 01-map.js
@@ -0,0 +1,9 @@
+const students =[
+    {id: 21, name:"Omar Faruk"},
+    {id: 31, name:"Mannnna"},
+    {id: 41, name:"Moyuri "},
+    {id: 51, name:"Deep Jol"}
+]
+
+let names=students.map(x=>x.name);
+console.log(names);
